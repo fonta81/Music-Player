ad4eaa0f0b808376e0599ebacc4cdeea2cf35b1c
mteo <cortinamateo211@gmail.com>
2026-07-25 08:49:48 -0500

    Frist
    Lice

---
+++ App.py
@@ -0,0 +1,26 @@
+from textual.app import App, ComposeResult
+from textual.binding import Binding
+from textual.widgets import Footer, Header
+
+
+class Reproductor(App):
+    BINDINGS = [
+        Binding(key="q", action="quit", description="Quit the app"),
+        Binding(
+            key="question_mark",
+            action="help",
+            description="Show help screen",
+            key_display="?",
+        ),
+        Binding(key="delete", action="delete", description="Delete the thing"),
+        Binding(key="j", action="down", description="Scroll down", show=False),
+    ]
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Footer()
+
+
+if __name__ == "__main__":
+    app = Reproductor()
+    app.run()
